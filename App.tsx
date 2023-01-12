772f4bbbd706dcb44d398cdafcc327ab98c6ed9f
pedro <pedrohhss14@gmail.com>
2023-01-12 07:49:34 -0300

    Feat: create default folder and make first build in expo go

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { ThemeContext } from "./src/context/ThemeContext";
+import { myColors } from "./src/style/Color";
+import Button from "./src/components/Button";
 
 export default function App() {
+  const [theme, setTheme] = useState(`light`);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <View
+        style={
+          theme === `light`
+            ? styles.container
+            : [styles.container, { backgroundColor: `#303030` }]
+        }
+      >
+        <Text>salve salve fml</Text>
+        <StatusBar style="auto" />
+        <Switch
+          value={theme === `light`}
+          onValueChange={() => setTheme(theme === `light` ? `dark` : `light`)}
+        />
+        <Button title="hello" onPress={() => {alert(`asdasd`)}} />
+      </View>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: myColors.light,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
